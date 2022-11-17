2fa2fa0618aa33d2642f0990169a039a24f47d19
viviashilah14 <viviashilah14@gmail.com>
2022-11-17 22:44:10 +0700

    mobile

---
+++ lib/braintechapp/generatedandroidsmall7widget/GeneratedAndroidSmall7Widget.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall7widget/generated/GeneratedMentoredbyWidget.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall7widget/generated/GeneratedLine4Widget.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall7widget/generated/GeneratedOliviaRodrigueWidget.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall7widget/generated/GeneratedProjectWidget.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall7widget/generated/GeneratedPngfind2Widget3.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall7widget/generated/GeneratedProfessionalPhotographerWidget.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall7widget/generated/GeneratedPlayLessonWidget.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall7widget/generated/Generated45Widget.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall7widget/generated/GeneratedLessonWidget.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall7widget/generated/GeneratedDuringDivorceFocusOnWhatYouWant3Widget.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall7widget/generated/Generated30kStudentsWidget.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall7widget/generated/GeneratedPngfind1Widget4.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall7widget/generated/GeneratedPhotographyCaptureandSharedYourLifeWidget2.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall7widget/generated/GeneratedIcons8save642Widget.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall7widget/generated/GeneratedRectangle13Widget.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall7widget/generated/GeneratedRectangle14Widget.dart';
+
+class GeneratedAndroidSmall7Widget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        child: ClipRRect(
+      borderRadius: BorderRadius.circular(30.0),
+      child: Container(
+        width: 360.0,
+        height: 640.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        child:
+            Stack(fit: StackFit.expand, alignment: Alignment.center, children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(30.0),
+            child: Container(
+              color: Color.fromARGB(255, 233, 231, 234),
+            ),
+          ),
+          Positioned(
+            left: -255.0,
+            top: -445.0,
+            right: null,
+            bottom: null,
+            width: 20.0,
+            height: 34.9376106262207,
+            child: GeneratedPngfind1Widget4(),
+          ),
+          Positioned(
+            left: 31.0,
+            top: 33.0,
+            right: null,
+            bottom: null,
+            width: 11.448980331420898,
+            height: 20.0,
+            child: GeneratedPngfind2Widget3(),
+          ),
+          Positioned(
+            left: 34.0,
+            top: 75.0,
+            right: null,
+            bottom: null,
+            width: 291.0,
+            height: 221.0,
+            child: GeneratedDuringDivorceFocusOnWhatYouWant3Widget(),
+          ),
+          Positioned(
+            left: 297.0,
+            top: 28.0,
+            right: null,
+            bottom: null,
+            width: 25.0,
+            height: 25.0,
+            child: GeneratedIcons8save642Widget(),
+          ),
+          Positioned(
+            left: 48.0,
+            top: 282.0,
+            right: null,
+            bottom: null,
+            width: 262.0,
+            height: 53.0,
+            child: GeneratedRectangle13Widget(),
+          ),
+          Positioned(
+            left: 64.0,
+            top: 293.0,
+            right: null,
+            bottom: null,
+            width: 230.0,
+            height: 42.0,
+            child: GeneratedRectangle14Widget(),
+          ),
+          Positioned(
+            left: 68.0,
+            top: 293.0,
+            right: null,
+            bottom: null,
+            width: 210.0,
+            height: 45.0,
+            child: GeneratedPlayLessonWidget(),
+          ),
+          Positioned(
+            left: 37.0,
+            top: 352.0,
+            right: null,
+            bottom: null,
+            width: 228.0,
+            height: 48.0,
+            child: GeneratedPhotographyCaptureandSharedYourLifeWidget2(),
+          ),
+          Positioned(
+            left: 55.0,
+            top: 590.0,
+            right: null,
+            bottom: null,
+            width: 85.0,
+            height: 23.0,
+            child: GeneratedLessonWidget(),
+          ),
+          Positioned(
+            left: 91.0,
+            top: 487.0,
+            right: null,
+            bottom: null,
+            width: 142.0,
+            height: 20.0,
+            child: GeneratedOliviaRodrigueWidget(),
+          ),
+          Positioned(
+            left: 36.0,
+            top: 456.0,
+            right: null,
+            bottom: null,
+            width: 120.0,
+            height: 20.0,
+            child: GeneratedMentoredbyWidget(),
+          ),
+          Positioned(
+            left: 78.0,
+            top: 409.0,
+            right: null,
+            bottom: null,
+            width: 101.0,
+            height: 20.0,
+            child: Generated30kStudentsWidget(),
+          ),
+          Positioned(
+            left: 91.0,
+            top: 505.0,
+            right: null,
+            bottom: null,
+            width: 183.0,
+            height: 20.0,
+            child: GeneratedProfessionalPhotographerWidget(),
+          ),
+          Positioned(
+            left: 257.0,
+            top: 409.0,
+            right: null,
+            bottom: null,
+            width: 104.0,
+            height: 23.0,
+            child: Generated45Widget(),
+          ),
+          Positioned(
+            left: 217.0,
+            top: 590.0,
+            right: null,
+            bottom: null,
+            width: 85.0,
+            height: 23.0,
+            child: GeneratedProjectWidget(),
+          ),
+          Positioned(
+            left: 34.0,
+            top: 617.0,
+            right: null,
+            bottom: null,
+            width: 100.0,
+            height: 2.0,
+            child: GeneratedLine4Widget(),
+          )
+        ]),
+      ),
+    ));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall1widget/GeneratedAndroidSmall1Widget.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall2widget/GeneratedAndroidSmall2Widget.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall4widget/GeneratedAndroidSmall4Widget.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall5widget/GeneratedAndroidSmall5Widget.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall6widget/GeneratedAndroidSmall6Widget.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall3widget/GeneratedAndroidSmall3Widget.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall7widget/GeneratedAndroidSmall7Widget.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall8widget/GeneratedAndroidSmall8Widget.dart';
+import 'package:flutterapp/braintechapp/generated211876_play_icon1widget/Generated211876_play_icon1Widget.dart';
+import 'package:flutterapp/braintechapp/generated1564507_checked_favorite_star_favourite_rating_icon1wid/Generated1564507_checked_favorite_star_favourite_rating_icon1Wid.dart';
+import 'package:flutterapp/braintechapp/generated309041_users_group_people_icon1widget/Generated309041_users_group_people_icon1Widget.dart';
+import 'package:flutterapp/braintechapp/generated20weirdthingsthatonlygirlswhowearglassescanrelateto1wid/Generated20WeirdThingsThatOnlyGirlsWhoWearGlassesCanRelateTo1Wid.dart';
+import 'package:flutterapp/braintechapp/generated2203510_favorite_favourite_heart_love_icon1widget/Generated2203510_favorite_favourite_heart_love_icon1Widget.dart';
+import 'package:flutterapp/braintechapp/generated2203510_favorite_favourite_heart_love_icon2widget/Generated2203510_favorite_favourite_heart_love_icon2Widget.dart';
+import 'package:flutterapp/braintechapp/generated2203510_favorite_favourite_heart_love_icon4widget/Generated2203510_favorite_favourite_heart_love_icon4Widget.dart';
+import 'package:flutterapp/braintechapp/generated2203510_favorite_favourite_heart_love_icon3widget/Generated2203510_favorite_favourite_heart_love_icon3Widget.dart';
+import 'package:flutterapp/braintechapp/generated2216353_location_locator_map_marker_pin_icon1widget/Generated2216353_location_locator_map_marker_pin_icon1Widget.dart';
+import 'package:flutterapp/braintechapp/generated2216353_location_locator_map_marker_pin_icon2widget/Generated2216353_location_locator_map_marker_pin_icon2Widget.dart';
+import 'package:flutterapp/braintechapp/generatedpremiumvector_womanworkingatlaptopfromhomeillustration1/GeneratedPremiumVector_Womanworkingatlaptopfromhomeillustration1.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall9widget/GeneratedAndroidSmall9Widget.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall10widget/GeneratedAndroidSmall10Widget.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall11widget/GeneratedAndroidSmall11Widget.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall12widget/GeneratedAndroidSmall12Widget.dart';
+import 'package:flutterapp/braintechapp/generatedandroidsmall13widget/GeneratedAndroidSmall13Widget.dart';
+import 'package:flutterapp/braintechapp/generatedduringdivorcefocusonwhatyouwant1widget1/GeneratedDuringDivorceFocusOnWhatYouWant1Widget1.dart';
+import 'package:flutterapp/braintechapp/generated3994400_arrow_forward_navigation_next_right_icon1widget/Generated3994400_arrow_forward_navigation_next_right_icon1Widget.dart';
+import 'package:flutterapp/braintechapp/generated3994400_arrow_forward_navigation_next_right_icon2widget/Generated3994400_arrow_forward_navigation_next_right_icon2Widget.dart';
+import 'package:flutterapp/braintechapp/generated3994400_arrow_forward_navigation_next_right_icon3widget/Generated3994400_arrow_forward_navigation_next_right_icon3Widget.dart';
+import 'package:flutterapp/braintechapp/generated3994400_arrow_forward_navigation_next_right_icon4widget/Generated3994400_arrow_forward_navigation_next_right_icon4Widget.dart';
+
+void main() {
+  runApp(BraintechApp());
+}
+
+class BraintechApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedAndroidSmall1Widget',
+      routes: {
+        '/GeneratedAndroidSmall1Widget': (context) =>
+            GeneratedAndroidSmall1Widget(),
+        '/GeneratedAndroidSmall2Widget': (context) =>
+            GeneratedAndroidSmall2Widget(),
+        '/GeneratedAndroidSmall4Widget': (context) =>
+            GeneratedAndroidSmall4Widget(),
+        '/GeneratedAndroidSmall5Widget': (context) =>
+            GeneratedAndroidSmall5Widget(),
+        '/GeneratedAndroidSmall6Widget': (context) =>
+            GeneratedAndroidSmall6Widget(),
+        '/GeneratedAndroidSmall3Widget': (context) =>
+            GeneratedAndroidSmall3Widget(),
+        '/GeneratedAndroidSmall7Widget': (context) =>
+            GeneratedAndroidSmall7Widget(),
+        '/GeneratedAndroidSmall8Widget': (context) =>
+            GeneratedAndroidSmall8Widget(),
+        '/Generated211876_play_icon1Widget': (context) =>
+            Generated211876_play_icon1Widget(),
+        '/Generated1564507_checked_favorite_star_favourite_rating_icon1Wid':
+            (context) =>
+                Generated1564507_checked_favorite_star_favourite_rating_icon1Wid(),
+        '/Generated309041_users_group_people_icon1Widget': (context) =>
+            Generated309041_users_group_people_icon1Widget(),
+        '/Generated20WeirdThingsThatOnlyGirlsWhoWearGlassesCanRelateTo1Wid':
+            (context) =>
+                Generated20WeirdThingsThatOnlyGirlsWhoWearGlassesCanRelateTo1Wid(),
+        '/Generated2203510_favorite_favourite_heart_love_icon1Widget':
+            (context) =>
+                Generated2203510_favorite_favourite_heart_love_icon1Widget(),
+        '/Generated2203510_favorite_favourite_heart_love_icon2Widget':
+            (context) =>
+                Generated2203510_favorite_favourite_heart_love_icon2Widget(),
+        '/Generated2203510_favorite_favourite_heart_love_icon4Widget':
+            (context) =>
+                Generated2203510_favorite_favourite_heart_love_icon4Widget(),
+        '/Generated2203510_favorite_favourite_heart_love_icon3Widget':
+            (context) =>
+                Generated2203510_favorite_favourite_heart_love_icon3Widget(),
+        '/Generated2216353_location_locator_map_marker_pin_icon1Widget':
+            (context) =>
+                Generated2216353_location_locator_map_marker_pin_icon1Widget(),
+        '/Generated2216353_location_locator_map_marker_pin_icon2Widget':
+            (context) =>
+                Generated2216353_location_locator_map_marker_pin_icon2Widget(),
+        '/GeneratedPremiumVector_Womanworkingatlaptopfromhomeillustration1':
+            (context) =>
+                GeneratedPremiumVector_Womanworkingatlaptopfromhomeillustration1(),
+        '/GeneratedAndroidSmall9Widget': (context) =>
+            GeneratedAndroidSmall9Widget(),
+        '/GeneratedAndroidSmall10Widget': (context) =>
+            GeneratedAndroidSmall10Widget(),
+        '/GeneratedAndroidSmall11Widget': (context) =>
+            GeneratedAndroidSmall11Widget(),
+        '/GeneratedAndroidSmall12Widget': (context) =>
+            GeneratedAndroidSmall12Widget(),
+        '/GeneratedAndroidSmall13Widget': (context) =>
+            GeneratedAndroidSmall13Widget(),
+        '/GeneratedDuringDivorceFocusOnWhatYouWant1Widget1': (context) =>
+            GeneratedDuringDivorceFocusOnWhatYouWant1Widget1(),
+        '/Generated3994400_arrow_forward_navigation_next_right_icon1Widget':
+            (context) =>
+                Generated3994400_arrow_forward_navigation_next_right_icon1Widget(),
+        '/Generated3994400_arrow_forward_navigation_next_right_icon2Widget':
+            (context) =>
+                Generated3994400_arrow_forward_navigation_next_right_icon2Widget(),
+        '/Generated3994400_arrow_forward_navigation_next_right_icon3Widget':
+            (context) =>
+                Generated3994400_arrow_forward_navigation_next_right_icon3Widget(),
+        '/Generated3994400_arrow_forward_navigation_next_right_icon4Widget':
+            (context) =>
+                Generated3994400_arrow_forward_navigation_next_right_icon4Widget(),
+      },
+    );
+  }
+}
